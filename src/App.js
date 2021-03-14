ff8dcdb6cfda999b3422334145add70ddb639c7d
ZMAN <zachh@github.com>
2021-03-14 11:35:22 -0500

    added header component sass stylings and logo

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/Shop/shop.component';
+import Header from './components/Header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
