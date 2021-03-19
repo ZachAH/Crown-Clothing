46d68ed1b82f02daa358f60f7540d5ab726518da
ZMAN <zachh@github.com>
2021-03-19 11:15:46 -0500

    added function to firebase.utils to create user in database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage.component';
 import ShopPage from './pages/Shop/shop.component';
 import Header from './components/Header/header.component';
 import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -23,8 +23,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
 
       console.log(user);
     });
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,22 +3,53 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAqvr4mNPTgAFZEo7CRYGIZXYH8Vwu2uAw",
-    authDomain: "crwn-db-b2375.firebaseapp.com",
-    projectId: "crwn-db-b2375",
-    storageBucket: "crwn-db-b2375.appspot.com",
-    messagingSenderId: "504684808282",
-    appId: "1:504684808282:web:e04d44c085e007fa03f063",
-    measurementId: "G-7S0K3061HW"
-  };
+  apiKey: "AIzaSyAqvr4mNPTgAFZEo7CRYGIZXYH8Vwu2uAw",
+  authDomain: "crwn-db-b2375.firebaseapp.com",
+  projectId: "crwn-db-b2375",
+  storageBucket: "crwn-db-b2375.appspot.com",
+  messagingSenderId: "504684808282",
+  appId: "1:504684808282:web:e04d44c085e007fa03f063",
+  measurementId: "G-7S0K3061HW"
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
-  
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-  
-  export default firebase;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message);
+
+    }
+  }
+
+  return userRef;
+
+};
+
+
+
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
