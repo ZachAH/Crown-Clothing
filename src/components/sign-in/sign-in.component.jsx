5473bbcab5720fce246ee8dc7f16de4478ec048e
ZMAN <zachh@github.com>
2021-03-20 11:21:42 -0500

    finished sign-in handlesubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,41 +1,48 @@
 import React from 'react';
 import FormInput from '../form-input/from-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: ''
-        };
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    handleSubmit = event => {
-        event.PreventDefault();
+  handleChange = event => {
+    const { value, name } = event.target;
 
-        this.setState({ email: '', password: ' ' })
-    };
+    this.setState({ [name]: value });
+  };
 
-    handleChange = event => {
-        const { value, name } = event.target;
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-        this.setState({ [name]: value })
-    };
-
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <FormInput
             name='email'
             type='email'
