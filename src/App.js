64aaadab80feaaf05396ee09e75e2347f3d29157
ZMAN <zachh@github.com>
2021-03-24 11:39:30 -0500

    implemented user reducer and useractions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,38 +9,33 @@ import ShopPage from './pages/Shop/shop.component';
 import Header from './components/Header/header.component';
 import SignInandSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
 
   unsubscribeFromAuth = null;
 
 componentDidMount() {
+  const {setCurrentUser} = this.props;
+
   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
       userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
+        setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
-          }
+          });
         });
-      });
-    } 
-    //wrap in else block so setState doesn't fire twice
-    this.setState({currentUser: userAuth});
-  });
-}
+      } 
+      //wrap in else block so setState doesn't fire twice
+      setCurrentUser(userAuth);
+    });
+  }
+          
 
 componentWillUnmount() {
   this.unsubscribeFromAuth();
@@ -59,4 +55,8 @@ render() {
 }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
