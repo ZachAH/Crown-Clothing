d19d93ad097addeb765246c299e70313f4ddf428
ZMAN <zachh@github.com>
2021-04-04 16:20:25 -0500

    renamed category to collection page added in page routing

--- src/pages/Shop/shop.component.jsx
+++ src/pages/Shop/shop.component.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import {Route} from 'react-router-dom';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../../pages/category/collection.component';
 
 
 
-import CollectionPreview from '../../components/preview-collection/collection-preview';
-import {selectCollections} from '../../redux/shop/shop.selectors';
 
-const ShopPage = ({collections}) => (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
+const ShopPage = ({ match }) => (
+ <div className='shop-page'>
+    < Route exact path={`${match.path}`} component={CollectionsOverview}  />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-    const mapStateToProps = createStructuredSelector({
-      collections: selectCollections
-    });
-    
-    export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
 
 
-  
